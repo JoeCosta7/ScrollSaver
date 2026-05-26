8ecb4e172bf5c6f206599530016e4752aed3cd02
JoeCosta7 <joedc3711@gmail.com>
2026-05-26 15:38:10 -0400

    it mostly works so version 1.1

--- ScrollSaver/content.js
+++ ScrollSaver/content.js
@@ -2,26 +2,29 @@
 chrome.storage.local.get(['mySavedUrls', 'scrollPositions'], async function(result) {
     const existingUrls = result.mySavedUrls || [];
     if (existingUrls.includes(location.href)) {
+        index = existingUrls.indexOf(location.href);
         const newElement = document.createElement("div");
         newElement.setAttribute("id", "newAnchor");
         newElement.textContent = "Anchor is here...";
         newElement.style.position = "absolute";
         newElement.style.left = "0px";
-        newElement.style.top = result.scrollPositions[1] + "px";
+        newElement.style.top = result.scrollPositions[index][1] + "px";
         newElement.style.backgroundColor = "red";
         newElement.style.padding = "10px";
         newElement.style.zIndex = "10000";
 
         document.body.appendChild(newElement);
         if(result.scrollPositions){
-            window.scrollTo(result.scrollPositions[0], result.scrollPositions[1]);
+            window.addEventListener('load', () => {
+                window.scrollTo(result.scrollPositions[index][0], result.scrollPositions[index][1]);
+            });
         }
     }
 
     window.navigation.addEventListener("navigate", () =>{
         console.log('location changed!');
+        const anchor = document.getElementById("newAnchor");
         if (existingUrls.includes(location.href)){
-            const anchor = document.getElementById("newAnchor");
             if (anchor) {
                 anchor.style.visibility = "hidden";
             }
