6cbc9bfc7448c010d8982a37fad4009bf16f7212
fishpyramid <hihihican@gmail.com>
2026-05-27 22:05:29 -0400

    multiple anchors per page

--- ScrollSaver/content.js
+++ ScrollSaver/content.js
@@ -1,38 +1,37 @@
 
-chrome.storage.local.get(['mySavedUrls', 'scrollPositions'], async function(result) {
-    const existingUrls = result.mySavedUrls || [];
-    if (existingUrls.includes(location.href)) {
-        index = existingUrls.indexOf(location.href);
-        const newElement = document.createElement("div");
-        newElement.setAttribute("id", "newAnchor");
-        newElement.textContent = "Anchor is here...";
-        newElement.style.position = "absolute";
-        newElement.style.left = "0px";
-        newElement.style.top = result.scrollPositions[index][1] + "px";
-        newElement.style.backgroundColor = "red";
-        newElement.style.padding = "10px";
-        newElement.style.zIndex = "10000";
+chrome.storage.local.get(['saves'], async function(result) {
+    const existingUrls = result.saves || [];
+    const entry = existingUrls.find(ent => ent.url === location.href)
+    if (entry) {
+        entry.positions.forEach((pos) => {
+            const index = existingUrls.indexOf(location.href);
+            const newElement = document.createElement("div");
+            newElement.classList.add("saved-anchor-marker");
+            newElement.setAttribute("id", `anchor-${pos[0]}-${pos[1]}`);
+            newElement.textContent = `Anchor ${pos[0]}, ${pos[1]}`;
+            newElement.style.position = "absolute";
+            newElement.style.left = "0px";
+            newElement.style.top = pos[1] + "px";
+            newElement.style.backgroundColor = "red";
+            newElement.style.padding = "10px";
+            newElement.style.zIndex = "10000";
 
-        document.body.appendChild(newElement);
-        // if(result.scrollPositions){
-        //     window.addEventListener('load', () => {
-        //         window.scrollTo(result.scrollPositions[index][0], result.scrollPositions[index][1]);
-        //     });
-        // }
+            document.body.appendChild(newElement);
+        });
     }
 
     window.navigation.addEventListener("navigate", () =>{
         console.log('location changed!');
-        const anchor = document.getElementById("newAnchor");
-        if (existingUrls.includes(location.href)){
-            if (anchor) {
+        const anchors = document.querySelectorAll(".saved-anchor-marker");
+        const urlMatchExists = existingUrls.some(ent => ent.url === location.href);
+
+        anchors.forEach(anchor => {
+            if (urlMatchExists){
                 anchor.style.visibility = "hidden";
-            }
-        } else {
-            if (anchor) {
+            } else {
                 anchor.style.visibility = "visible";
             }
-        }
+        });
     });
 });
 
